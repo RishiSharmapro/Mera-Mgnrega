b812078702e30ede502a74d011a6a4aab729f5de
Rishi Sharma <rishi.sharma4945@gmail.com>
2025-10-28 15:44:47 +0530

    feat: Implement database seeding from processed district data

---
+++ backend/db/seed_from_processed_data.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { District } from '../models/district.model.js';
+
+const __dirname = path.resolve()
+const filePath = path.join(__dirname, "/db/processed_data.json");
+
+const seedDatabase = async () => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const allDistricts = JSON.parse(data);
+        
+        console.log(`📦 Preparing to insert ${allDistricts.length} districts...`);
+
+        // await District.deleteMany({});
+        for (const district of allDistricts) {
+            if (district.district_code && district.district_name) {
+                await District.insertOne(district);
+            }
+        }
+        // await District.insertMany(allDistricts);
+        console.log("✅ Data seeded successfully!");
+    } catch (error) {
+        console.error("Error reading or parsing data.json:", error);
+    }
+}
+// seedDatabase();
+
+export { seedDatabase };
